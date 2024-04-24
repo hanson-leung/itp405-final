211c77ca6f9f0297f331684a22982fcb5c833674
Hanson Leung <53881772+hanson-leung@users.noreply.github.com>
2024-04-24 16:51:10 -0700

    add more migrations

--- database/migrations/2024_04_24_234941_create_friends_table.php
+++ database/migrations/2024_04_24_234941_create_friends_table.php
@@ -11,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_events_bridge', function (Blueprint $table) {
+        Schema::create('friends', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
 
-            $table->integer('event_id')->unsigned();
-            $table->foreign('event_id')->references('id')->on('events');
+            $table->integer('friend_id')->unsigned();
+            $table->foreign('friend_id')->references('id')->on('users');
         });
     }
 
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_events_bridge');
+        Schema::dropIfExists('friends');
     }
 };
